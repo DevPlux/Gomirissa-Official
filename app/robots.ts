c3cb4eb82a85fab67e335fcf91e8a60637fcf2d9
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-07-13 15:06:51 +0530

    Updated

--- app/robots.ts
+++ app/robots.ts
@@ -8,6 +8,6 @@ export default function robots(): MetadataRoute.Robots {
         allow: "/",
       },
     ],
-    sitemap: "https://www.gomirissa.com/sitemap.xml",
+    sitemap: "https://gomirissa.com/sitemap.xml",
   };
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://www.gomirissa.com";
+  const baseUrl = "https://gomirissa.com";
 
   return [
     {

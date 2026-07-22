252c7106f6647f1e6ae367373541ee7c13e70db1
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-07-22 15:17:00 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,32 +1,15 @@
-import { MetadataRoute } from "next";
+// app/sitemap.ts
+
+import type { MetadataRoute } from "next";
+
+const siteUrl = "https://www.gomirissa.com";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://www.gomirissa.com";
-
   return [
     {
-      url: baseUrl,
-      lastModified: new Date(),
+      url: `${siteUrl}/`,
       changeFrequency: "weekly",
       priority: 1,
-    },
-    {
-      url: `${baseUrl}/login`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.6,
-    },
-    {
-      url: `${baseUrl}/register`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.6,
-    },
-    {
-      url: `${baseUrl}/my-bookings`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.7,
     },
   ];
 }
